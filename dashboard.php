2e7d1d272deccea94f9c267c462a3a075663e62b
Henkmbo <90178722+Henkmbo@users.noreply.github.com>
2022-04-06 14:02:46 +0200

    nieuwe dashboard

--- dashboard.php
+++ dashboard.php
@@ -11,6 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="./favicon (1).ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 </head>
 
 <body>
@@ -50,6 +51,7 @@
 
 
 <!-- CONTENT -->
+
 <div class="uploaden">
 
 
@@ -66,7 +68,7 @@
 
        <div class="mb-3">
        <label for="inputTitel">Titel:</label>
-       <input class="form-control" placeholder="invoeren is verplicht...." type="text" name="titel" id="inputTitel" required> </br>
+       <input class="form-control" placeholder="Invoeren is verplicht...." type="text" name="titel" id="inputTitel" required> </br>
        </div>
        
        <div class="mb-3">
@@ -76,56 +78,14 @@
 
        <div class="mb-3">
        <label for="inputImage_url">image_url:</label>
-       <input class="form-control" type="text" name="image_url" id="inputImage_url"> </br>
+       <input class="form-control" type="text" name="image_url" id="inputImage_url" placeholder="Invoeren is verplicht...." required> </br>
        </div>
 
+       <br>
        
-       
-           <input type="submit" name="submit" value="Upload">
-
-       
-
-     </form>
-     
- </div>
-
-       
-
- </div>
-    
-    
-</body>
-
-        f
-
-      </form>
-      
-  </div>
-
-        
-
-  </div>
-
+           <input  class="button" type="submit" name="submit" value="Upload">
 </body>
 </html>
-
-
-
-
-<!-- FOOTER -->
-  <div class="footer">
-    <div id="button"></div>
-  <div id="container">
-  <div id="cont">
-  <div class="footer_center">
-       <h3></h3>
-  </div>
-  </div>
-  </div>
-  </div>
-
-
-
 
 <style>
 .uploaden{
@@ -134,6 +94,37 @@
     align-items: center;
     flex-wrap: wrap;
     min-height: 100vh;
+    font-size: 18px;
+    box-sizing: border-box;
+    border: 8px solid green;
+    background-color: lightgrey;
+  border: 6px solid green;
+  padding: 50px;
+  margin: 20px;
+
+}
+.button{
+font-size: 16px;
+    letter-spacing: 2px;
+    font-weight: 400 !important;
+    background-color: grey;
+    color: black;
+    padding: 15px 40px;
+    margin: auto;
+    text-align: center;
+    display: inline-block !important;
+    text-decoration: none;
+    border: 0px;
+    width: max-content;
+    cursor: pointer;
+    transition: all 0.3s 0s ease-in-out;
+  }
+  
+  
+.button:hover {
+    transform: scale(1.04, 1);
+    background-color: green;
+    text-decoration: none;
 }
 .alb{
     width: 200px;
@@ -146,6 +137,7 @@
     height: 100%;
     
 }
+
 </style>
 <script>
   var navList = document.getElementById("nav-lists");
@@ -158,6 +150,6 @@
   }
   </script>
 
-</body>
+
 
 </html>
